c8b9bf0265905f1f664277a8def404fc58438355
gnoerrana <gnoerrana@gmail.com>
2026-06-29 12:27:28 +0700

    enhance email receipt send, adjust payment logic

--- wp-content/plugins/csc/includes/class-payment-method-api.php
+++ wp-content/plugins/csc/includes/class-payment-method-api.php
@@ -74,7 +74,7 @@ class CSC_Payment_Method_API {
         $payment_methods_data = $settings['payment_methods_data'] ?? array();
 
         $available_methods = array(
-            'credit_card' => 'Online Payment',
+            'online_payment' => 'Online Payment',
             'bank_transfer' => 'Bank Transfer',
             'qris' => 'QRIS',
             'cash_in_shop' => 'Cash in Shop',
